c7861b5fa43bb96473e0f721c05b460c064a6cde
Alden Wu <aldenwu0@gmail.com>
2024-06-17 21:32:30 -0700

    Update about.tsx

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -10,7 +10,7 @@ export class About extends React.Component {
             <Components.Link to="https://github.com/goodtrailer/rote">https://github.com/goodtrailer/rote</Components.Link>
             <Components.Link to="/users/goodtrailer">/users/goodtrailer</Components.Link>
             <Joy.Typography style={{ marginTop: 20, maxWidth: "60%", minWidth: 400 }}>
-                Hello! This is the first website I've ever made. It took roughly two weeks, much longer than anticipated. I mostly stick to desktop and low-level programming, so this has been quite the learning experience. Check out the GitHub repository for a rough outline of the tech stack I used.
+                Hello! This is the first website I've ever made. It took roughly two weeks, much longer than anticipated. I feel like this is the kind of website you might see at a hackathon, and it only takes them a couple days. I mostly stick to desktop and low-level programming, so this has been quite the learning experience. Check out the GitHub repository for a rough outline of the tech stack I used.
             </Joy.Typography>
         </Components.Wrapper>;
     };
